b57448bc3a467db5b973844dd852210b73cfd565
Xiao Ma <selkie@outlook.com>
2014-02-17 19:23:00 -0500

    Initial Commit

---
+++ CAParallaxTopView/CAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CAAppDelegate.h
+//  CAParallaxTopView
+//
+//  Created by Xiao Ma on 2/17/2014.
+//  Copyright (c) 2014 Xiao Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CAParallaxTopView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CAParallaxTopView
+//
+//  Created by Xiao Ma on 2/17/2014.
+//  Copyright (c) 2014 Xiao Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CAAppDelegate class]));
+    }
+}
